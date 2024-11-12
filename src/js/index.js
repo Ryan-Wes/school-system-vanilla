f5767cdf64f19830f084c74ccb3702d88f137858
WRyan <wesley.ryan03@gmail.com>
2024-11-11 21:05:51 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,40 @@
-const makeSelect = $('#make-select');
-const yearSelect = $('#year-select');
-const nextButton = $('#next-button');
-
-fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json')
-    .then(response => response.json())
-    .then(data => {
-        const sortedMakes = data.Results.sort((a, b) => a.MakeName.localeCompare(b.MakeName));
-
-        sortedMakes.forEach(make => {
-            const option = $('<option></option>').val(make.MakeId).text(make.MakeName);
-            makeSelect.append(option);
-        });
-
-    });
-
-const currentYear = new Date().getFullYear();
-for (let year = 2015; year <= currentYear; year++) {
-    const option = $('<option></option>').val(year).text(year);
-    yearSelect.append(option);
-}
-
-makeSelect.on('change', validateSelection);
-yearSelect.on('change', validateSelection);
-
-function validateSelection() {
-    if (makeSelect.val() && yearSelect.val()) {
-        nextButton.prop('disabled', false);
-    } else {
-        nextButton.prop('disabled', true);
-    }
-}
-
-nextButton.on('click', () => {
-    const makeId = makeSelect.val();
-    const year = yearSelect.val();
-    window.location.href = `results.html?makeId=${makeId}&year=${year}`;
+document.getElementById("entrarBtn").addEventListener("click", function () {
+    window.location.href = "login.html"; // Redireciona para a página de login
 });
+
+// Carrossel
+let currentIndex = 0; // Índice do item atual
+const items = document.querySelectorAll('.carousel-item'); // Seleciona todos os itens do carrossel
+const totalItems = items.length; // Total de itens
+
+// Função para atualizar a exibição do carrossel
+function updateCarousel() {
+    items.forEach((item, index) => {
+        // Remove a classe active de todos os itens
+        item.classList.remove('active');
+        // Adiciona a classe active apenas ao item atual
+        if (index === currentIndex) {
+            item.classList.add('active');
+        }
+    });
+}
+
+// Evento para o botão "Próximo"
+document.getElementById('nextBtn').addEventListener('click', function() {
+    // Atualiza o índice para o próximo item
+    currentIndex = (currentIndex + 1) % totalItems;
+    // Atualiza a exibição do carrossel
+    updateCarousel();
+});
+
+// Evento para o botão "Anterior"
+document.getElementById('prevBtn').addEventListener('click', function() {
+    // Atualiza o índice para o item anterior
+    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+    // Atualiza a exibição do carrossel
+    updateCarousel();
+});
+
+// Inicializa o carrossel ao carregar a página
+updateCarousel();
+
